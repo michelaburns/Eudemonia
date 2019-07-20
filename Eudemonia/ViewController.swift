39c6384c27b5848339843636347a667ff5967a75
Michela Burns <michelaburns@berkeley.edu>
2019-07-20 15:31:47 -0700

    Initial Commit

---
+++ Eudemonia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eudemonia
+//
+//  Created by Owner on 7/20/19.
+//  Copyright © 2019 Michela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
